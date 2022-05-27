b42ab239dbe340310114c7f75acc1a138db1b9e3
Andrew Goetz <agoetz4407@gmail.com>
2022-05-27 17:16:58 -0500

    category routes working correctly

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,6 +4,7 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  //find all categories
   Category.findAll({
     attributes: ['id', 'category_name']
   })
@@ -15,20 +16,79 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  //get with category with the id in the request url
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'category_name']
+  })
+  .then(categoryData => {
+    if (!categoryData) {
+      res.status(404).json({message: 'No category with the given id number'});
+      return;
+    }
+    res.json(categoryData);
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+    Category.create({
+        category_name: req.body.category_name
+    })
+    .then(categoryData => {
+        res.json({
+          message:'Category created!',
+          data: categoryData
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categoryData => {
+    if (!categoryData[0]) {
+      res.status(404).json({message: 'No category with the given id number'});
+      return;
+    }
+    res.json({
+      message: 'Category updated!',
+      changes: categoryData
+    })
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categoryData => {
+    if (!categoryData) {
+      res.status(404).json({message: 'No category with the given id number'});
+      return;
+    }
+    res.json({
+      message: 'Category deleted!',
+      changes: categoryData
+    })
+  })
 });
 
 module.exports = router;
