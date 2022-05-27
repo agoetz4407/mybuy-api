ccaefa18fb76df512c7dd779d3c42e62c7d72173
Andrew Goetz <agoetz4407@gmail.com>
2022-05-27 16:40:40 -0500

    starting routes - new branch

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,14 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    attributes: ['id', 'category_name']
+  })
+  .then(categoryData => res.json(categoryData))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 router.get('/:id', (req, res) => {
