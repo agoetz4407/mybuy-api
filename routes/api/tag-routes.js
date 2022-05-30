151223c38b3fe6782b2469308f35f1242c7c2fee
Andrew Goetz <agoetz4407@gmail.com>
2022-05-29 19:11:28 -0500

    tag CUD routes working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -58,14 +58,61 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then(tagData => {
+      res.json({
+        message:'Tag created!',
+        data: tagData
+      })
+  })
+  .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(tagData => {
+    if (!tagData[0]) {
+      res.status(404).json({message: 'No tag with the given id number'});
+      return;
+    }
+    res.json({
+      message:'Tag updated!',
+      data: tagData
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(tagData => {
+    if (!tagData) {
+      res.status(404).json({message: 'No tag with the given id number'});
+      return;
+    }
+    res.json({
+      message: 'Tag deleted!',
+      changes: tagData
+    })
+  })
 });
 
 module.exports = router;
